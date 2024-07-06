333b656fefa976d751afe7d70b540a6646ab6102
amandeepsingh1929 <as0632568@gmail.com>
2024-07-07 01:00:09 +0530

    Backend Starting with basics.

---
+++ index.js
@@ -0,0 +1,19 @@
+require('dotenv').config()
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get('/twitter',(req,res)=>{
+  res.send("twitter Called");
+})
+
+app.get('/login' ,(req,res)=>{
+  res.send('<h1>Login Please</h1>')
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${process.env.PORT}`);
+});
